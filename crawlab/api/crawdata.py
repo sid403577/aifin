9f190cfcbc8316a36a9bf3278b5e01b999149a9a
sid403577 <sid403577@gmail.com>
2023-07-15 11:31:29 +0800

    craw

--- crawlab/api/crawdata.py
+++ crawlab/api/crawdata.py
@@ -23,22 +23,35 @@ def buildMarketdata(stock:str,market:int):
     while True:
         print(f"开始获取第{page+1}页数据")
         url = f"https://b2b-news.10jqka.com.cn/hxcota/market/stocks/v2/list?stock={stock}&market={market}&type=1&accessKey=74f9abd518ab0970&page={page}&pageSize=250"
+        print(f"url:{url}")
         response = requests.get(url)
         if response.status_code==200:
             response_text = json.loads(response.text)
             if 'data' in response_text:
                 length = len(response_text['data'])
                 print(f"获取到{length}条数据")
+                print("----")
                 if length==0:
                     print(f"原因：{'' if 'msg' not in response_text else response_text['msg']}")
                     break
                 for pre_data in response_text['data']:
-                    print(f"")
+                    total += 1
+                    print(f"开始处理第【{total}】条数据")
                     digest = '' if 'digest' not in pre_data else pre_data['digest']
-                    if 'unique' in pre_data:
-                        id = pre_data['unique'].split("_")[-1]
+                    unique = '' if 'unique' not in pre_data else pre_data['unique']
+                    url = '' if 'url' not in pre_data else pre_data['url']
+                    sourceUrl = '' if 'sourceUrl' not in pre_data else pre_data['sourceUrl']
+                    detailUrl = '' if 'detailUrl' not in pre_data else pre_data['detailUrl']
+                    print(f"digest:{digest}")
+                    print(f"unique:{unique}")
+                    print(f"url:{url}")
+                    print(f"sourceUrl:{sourceUrl}")
+                    print(f"detailUrl:{detailUrl}")
+                    if len(unique)>0:
+                        id = unique.split("_")[-1]
                         if len(id)>0:
                             detail_url = f"https://b2b-news.10jqka.com.cn/hxcota/common/info/detail?id={id}&type=1&accessKey=74f9abd518ab0970"
+                            print(f"detail_url:{detail_url}")
                             response_detail = requests.get(detail_url)
                             if response_detail.status_code==200:
                                 response_detail_text = json.loads(response_detail.text)
@@ -58,11 +71,10 @@ def buildMarketdata(stock:str,market:int):
                                     print(f"title:{title}")
                                     print(f"rtime:{rtime}")
                                     print(f"source:{source}")
-                                    print(f"digest:{digest}")
                                     print(f"content:{content}")
                                     print(f"contentUrl:{contentUrl}")
-                                    total += 1
-                                    print(f"第{total}条数据处理完成")
+                    print(f"第{total}条数据处理完成")
+                    print("")
 
         page+=1
         print(f"第{page}页数据内容获取完毕")
