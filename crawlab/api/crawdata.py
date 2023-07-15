10ddf75a0ffdbd651bdbaeb52838699e392b25a2
sid403577 <sid403577@gmail.com>
2023-07-15 23:58:26 +0800

    craw

--- crawlab/api/crawdata.py
+++ crawlab/api/crawdata.py
@@ -1,10 +1,11 @@
-#获取单只股票的数据
+# 获取单只股票的数据
+import datetime
 import json
+
 import requests
 from bs4 import BeautifulSoup
-import datetime
 
-marketMap={
+marketMap = {
     48: "新同花顺指数",
     32: "深圳证券交易所",
     16: "上海证券交易所",
@@ -17,30 +18,31 @@ marketMap={
 }
 
 
-def buildMarketdata(stock:str,market:int):
+def buildMarketdata(stock: str, market: int):
     import csv
     # from crawlab import save_item
-    csv_file = open(f"/data/comments_data_{stock}_{market}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.csv", 'a+',
+    csv_file = open(f"/data/api_comments_data_{stock}_{market}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.csv",
+                    'a+',
                     newline='', encoding='utf-8-sig')  # 解决中文乱码问题。a+表示向csv文件追加
     writer = csv.writer(csv_file)
-    writer.writerow(['date', 'title', 'content', ])
+    writer.writerow(['id','date', 'title', 'content', ])
 
     page = 0
     total = 0
     print(f"========开始获取【{stock}】在【{marketMap[market]}】的数据")
     while True:
-        print(f"开始获取第{page+1}页数据")
-        url = f"https://b2b-news.10jqka.com.cn/hxcota/market/stocks/v2/list?stock={stock}&market={market}&type=1&accessKey=74f9abd518ab0970&page={page}&pageSize=10"
+        print(f"开始获取第{page + 1}页数据")
+        url = f"https://b2b-news.10jqka.com.cn/hxcota/market/stocks/v2/list?stock={stock}&market={market}&type=1&accessKey=74f9abd518ab0970&page={page}&pageSize=50"
         print(f"url:{url}")
         response = requests.get(url)
-        if response.status_code==200:
+        if response.status_code == 200:
             response_text = json.loads(response.text)
             if 'data' in response_text:
                 length = len(response_text['data'])
                 print(f"获取到{length}条数据")
                 print("----------------")
                 print("\n")
-                if length==0:
+                if length == 0:
                     print(f"原因：{'' if 'msg' not in response_text else response_text['msg']}")
                     break
                 storageList: list[Document] = []
@@ -58,13 +60,13 @@ def buildMarketdata(stock:str,market:int):
                         print(f"url:{url}")
                         print(f"sourceUrl:{sourceUrl}")
                         print(f"detailUrl:{detailUrl}")
-                        if len(unique)>0:
+                        if len(unique) > 0:
                             id = unique.split("_")[-1]
-                            if len(id)>0:
+                            if len(id) > 0:
                                 detail_url = f"https://b2b-news.10jqka.com.cn/hxcota/common/info/detail?id={id}&type=1&accessKey=74f9abd518ab0970"
                                 print(f"detail_url:{detail_url}")
                                 response_detail = requests.get(detail_url)
-                                if response_detail.status_code==200:
+                                if response_detail.status_code == 200:
                                     response_detail_text = json.loads(response_detail.text)
                                     if 'data' in response_detail_text:
                                         data = response_detail_text['data']
@@ -94,13 +96,14 @@ def buildMarketdata(stock:str,market:int):
                                         print(f"相关个股-stockInfo:{stockInfo}")
                                         s_date = time
                                         try:
-                                            s_date = datetime.datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S')
+                                            s_date = datetime.datetime.fromtimestamp(int(time)).strftime(
+                                                '%Y-%m-%d %H:%M:%S')
                                         except:
                                             print("发布时间解析异常，")
                                             s_date = time
                                         createTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                                         # 写入csv文件
-                                        result_item1 = [s_date, title, content]
+                                        result_item1 = [total,s_date, title, content]
                                         writer.writerow(result_item1)  # 原来的链接不全因此给他补齐
                                         # 写入矢量库
                                         doc = Document(page_content=content,
@@ -128,14 +131,11 @@ def buildMarketdata(stock:str,market:int):
                 print("hasNext==False，跳出循环")
                 break
 
-
     print(f"========获取【{stock}】在【{marketMap[market]}】的数据完毕，一共获取到【{total}】条数据")
-
+    csv_file.close()
 
 
 ###################### 存储类 ###############################################
-
-from typing import List
 
 import torch
 from langchain.docstore.document import Document
@@ -154,12 +154,14 @@ embedding_model_dict = {
 EMBEDDING_MODEL = "text2vec"
 EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
+
 def load_and_split(docs: list[Document]) -> list[Document]:
     print("进入切词阶段")
     """Load documents and split into chunks."""
-    _text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    _text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
     related_docs = _text_splitter.split_documents(docs)
-    return [doc for doc in related_docs if len(doc.page_content.strip()) > 20]
+    return [doc for doc in related_docs if len(doc.page_content.strip()) > 50]
+
 
 def store(docs: list[Document]):
     docs = load_and_split(docs)
@@ -167,20 +169,22 @@ def store(docs: list[Document]):
     embeddings = HuggingFaceEmbeddings(model_name=embedding_model_dict[EMBEDDING_MODEL],
                                        model_kwargs={'device': EMBEDDING_DEVICE})
     count = 0
-    while True and count<3:
+    while True and count < 3:
         try:
-            Milvus(collection_name="tonghuashun").from_documents(
+            Milvus.from_documents(
                 docs,
                 embeddings,
                 connection_args={"host": "8.217.52.63", "port": "19530"},
+                collection_name="tonghuashun",
             )
             break
-        except:
-            print("error,写入矢量库异常")
-            count+=1
+        except Exception as e:
+            print(f"error,写入矢量库异常,{e}")
+            count += 1
 
     print("over")
 
+
 if __name__ == '__main__':
     for market in marketMap:
-        buildMarketdata("002594",market)
+        buildMarketdata("002594", market)
