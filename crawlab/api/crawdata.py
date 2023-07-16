c0f549bc027564482d75fdf86110e14ae305dfc6
sid403577 <sid403577@gmail.com>
2023-07-16 20:59:22 +0800

    craw

--- crawlab/api/crawdata.py
+++ crawlab/api/crawdata.py
@@ -28,11 +28,11 @@ def buildMarketdata(stock: str, market: int):
     import csv
     # from crawlab import save_item
     csv_file = open(
-        f"/data/api_2_comments_data_{stock}_{market}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.csv",
+        f"/data/api_3_comments_data_{stock}_{market}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.csv",
         'a+',
         newline='', encoding='utf-8-sig')  # 解决中文乱码问题。a+表示向csv文件追加
     writer = csv.writer(csv_file)
-    writer.writerow(['id', 'date', 'title', 'content', 'url'])
+    writer.writerow(['id','uniqueId', 'date', 'title', 'content', 'url'])
 
     page = 0
     total = 0
@@ -58,17 +58,17 @@ def buildMarketdata(stock: str, market: int):
                         total += 1
                         print(f"开始处理第【{total}】条数据")
                         digest = '' if 'digest' not in pre_data else pre_data['digest']
-                        unique = '' if 'unique' not in pre_data else pre_data['unique']
+                        uniqueId = '' if 'unique' not in pre_data else pre_data['unique']
                         url = '' if 'url' not in pre_data else pre_data['url']
                         sourceUrl = '' if 'sourceUrl' not in pre_data else pre_data['sourceUrl']
                         detailUrl = '' if 'detailUrl' not in pre_data else pre_data['detailUrl']
                         print(f"digest:{digest}")
-                        print(f"unique:{unique}")
+                        print(f"unique:{uniqueId}")
                         print(f"url:{url}")
                         print(f"sourceUrl:{sourceUrl}")
                         print(f"detailUrl:{detailUrl}")
-                        if len(unique) > 0:
-                            id = unique.split("_")[-1]
+                        if len(uniqueId) > 0:
+                            id = uniqueId.split("_")[-1]
                             if len(id) > 0:
                                 detail_url = f"https://b2b-news.10jqka.com.cn/hxcota/common/info/detail?id={id}&type=1&accessKey=74f9abd518ab0970"
                                 print(f"detail_url:{detail_url}")
@@ -110,11 +110,12 @@ def buildMarketdata(stock: str, market: int):
                                             s_date = time
                                         createTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                                         # 写入csv文件
-                                        result_item1 = [total, s_date, title, content, url]
+                                        result_item1 = [total,uniqueId, s_date, title, content, url]
                                         writer.writerow(result_item1)  # 原来的链接不全因此给他补齐
                                         # 写入矢量库
                                         doc = Document(page_content=content,
                                                        metadata={"source": "API",
+                                                                 "uniqueId":uniqueId,
                                                                  "code": stock,
                                                                  "url": url,
                                                                  "date": s_date,
@@ -182,7 +183,7 @@ def store(docs: list[Document]):
                 docs,
                 embeddings,
                 connection_args={"host": "8.217.52.63", "port": "19530"},
-                collection_name="tonghuashun_2",
+                collection_name="tonghuashun_3",
             )
             break
         except Exception as e:
