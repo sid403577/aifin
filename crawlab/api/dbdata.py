a4de35f0bf0b8c8d2d43755ad9b813e44f9b2d5b
于洪杰 <yuhongjie@hangjf.com>
2023-07-17 16:36:44 +0800

    公司研报查询

--- crawlab/api/dbdata.py
+++ crawlab/api/dbdata.py
@@ -38,9 +38,10 @@ def store(docs: list[Document]):
     embeddings = HuggingFaceEmbeddings(model_name=embedding_model_dict[EMBEDDING_MODEL],
                                        model_kwargs={'device': EMBEDDING_DEVICE})
     count = 0
+    obj = None
     while True and count < 3:
         try:
-            Milvus.from_documents(
+            obj = Milvus.from_documents(
                 docs,
                 embeddings,
                 connection_args={"host": "8.217.52.63", "port": "19530"},
@@ -50,14 +51,15 @@ def store(docs: list[Document]):
         except Exception as e:
             print(f"error,写入矢量库异常,{e}")
             count += 1
-
+    if not obj:
+        raise Exception("写入矢量库异常")
     print("over")
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 # 连接到Elasticsearch实例
 def esBatch(docList:list):
-    es = Elasticsearch(['172.28.84.188:9200'])
+    es = Elasticsearch(['8.217.110.233:9200'])
     #es = Elasticsearch("http://192.168.1.1:9200", http_auth=('username', 'password'), timeout=20)
     index_name = 'aifin'
     if not es.indices.exists(index=index_name):
@@ -93,48 +95,57 @@ def transEs(security_code:str):
     connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
-    query = f"""
-    SELECT 'DB' as source,
-           a.SECURITYCODE as code,
-           a.EID as uniqueId,
-           '研报' as type,
-           a.COLUMNNAME as reTypeName,
-           a.SOURCEURL as url,
-           a.REPORT_TITLE_TRANSFER as title,
-           a.PUBLISHDATE as date,
-           a.SRATINGNAME as rawTating,
-           a.COMPANYNAME as comeName,
-           b.SEARCHCONTENT as abstract,
-           b.INFOBODYCONTENT as text
-    FROM INFO_RE_BASINFOCOM a
-    JOIN INFO_RE_CONTENTCOM b ON a.INFOCODE = b.INFOCODE
-    WHERE a.SECURITYCODE = '{security_code}'
-    ORDER BY a.PUBLISHDATE DESC
-    """
 
+    # 定义每页的大小和页码
+    page_size = 500
+    page_number = 1
+    # 计算要跳过的记录数量
+    skip_count = (page_number - 1) * page_size
     has_more_results = True
     while has_more_results:
+        query = f"""
+            SELECT 'DB' as source,
+                   a.SECURITYCODE as code,
+                   a.EID as uniqueId,
+                   '研报' as type,
+                   a.COLUMNNAME as reTypeName,
+                   a.SOURCEURL as url,
+                   a.REPORTTITLE as title,
+                   a.PUBLISHDATE as date,
+                   a.SRATINGNAME as rawTating,
+                   a.COMPANYNAME as comeName,
+                   b.SEARCHCONTENT as abstract,
+                   b.INFOBODYCONTENT as text
+            FROM INFO_RE_BASINFOCOM a
+            JOIN INFO_RE_CONTENTCOM b ON a.INFOCODE = b.INFOCODE
+            WHERE a.SECURITYCODE = '{security_code}'
+            and a.EID = 160000000744856202
+            ORDER BY a.PUBLISHDATE DESC
+            OFFSET {skip_count} ROWS FETCH NEXT {page_size} ROWS ONLY
+            """
         cursor.execute(query)
         results = cursor.fetchmany(500)  # Retrieve 500 records at a time
 
         if not results:
             has_more_results = False
             break
-
+        else:
+            page_number = page_number+1
+            skip_count = (page_number - 1) * page_size
         storageList: list[Document] = []
         esDocList: list = []
         for row in results:
             metadata = {
                 'source': row.source,
                 'code': row.code,
-                'uniqueId': row.uniqueId,
+                'uniqueId': f"{row.uniqueId}",
                 'type': row.type,
                 'reTypeName': row.reTypeName,
-                'url': row.url,
+                'url': row.url if row.url is not None else "",
                 'title': row.title,
                 'date': row.date,
                 'createTime': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                'rawTating': row.rawTating,
+                'rawTating': row.rawTating if row.rawTating is not None else "",
                 'comeName': row.comeName,
                 'abstract': row.abstract,
             }
@@ -162,6 +173,25 @@ def transEs(security_code:str):
 
 
 if __name__ == '__main__':
-    security_code = ['002594']
+    security_code = ['002594',
+		'600887',
+		'300750',
+		'002518',
+		'600225',
+		'600977',
+		'603259',
+		'300887',
+		'002967',
+		'002465',
+		'000063',
+		'600737',
+		'605196',
+		'300661',
+		'688536',
+		'060030',
+		'300418',
+		'002722',
+		'688312',
+		'03988']
     for row in security_code:
         docs = transEs(row)
