530f95e392b4faa86a8c96776aab711c58ca3a8f
sid403577 <sid403577@gmail.com>
2023-07-18 16:36:40 +0800

    矢量库

--- crawlab/storage/PreMilvusStore.py
+++ crawlab/storage/PreMilvusStore.py
@@ -1,43 +1,56 @@
+import sys
 
+###################### 存储类 ###############################################
 
-#############################FaissStore存储####################################
-import os
-import uuid
 import torch
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-
 from langchain.docstore.document import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Milvus
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+embedding_model_dict = {
+    "ernie-tiny": "nghuyong/ernie-3.0-nano-zh",
+    "ernie-base": "nghuyong/ernie-3.0-base-zh",
+    "text2vec-base": "shibing624/text2vec-base-chinese",
+    "text2vec": "/root/model/text2vec-large-chinese",
+    "m3e-small": "moka-ai/m3e-small",
+    "m3e-base": "moka-ai/m3e-base",
+}
+EMBEDDING_MODEL = "text2vec"
 EMBEDDING_DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
-# 知识检索内容相关度 Score, 数值范围约为0-1100，如果为0，则不生效，经测试设置为小于500时，匹配结果更精准
-VECTOR_SEARCH_SCORE_THRESHOLD = 0
-# 知识库默认存储路径
-KB_ROOT_PATH = "/data/store/knowledge_base"
-# 匹配后单段上下文长度
-CHUNK_SIZE = 250
 
-def temp_vector_store(vs_path, embeddings):
-    temp_vs_path = os.path.join(KB_ROOT_PATH, vs_path, "vector_store")
-    os.makedirs(temp_vs_path)
-    if os.path.isdir(vs_path) and "index.faiss" in os.listdir(vs_path):
-        return FAISS.load_local(temp_vs_path, embeddings)
-    docs = [Document(page_content="test", metadata={"source": "test", "url": "test"})]
-    return FAISS.from_documents(docs, embeddings)
+def load_and_split(docs: list[Document]) -> list[Document]:
+    print("进入切词阶段")
+    """Load documents and split into chunks."""
+    _text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    related_docs = _text_splitter.split_documents(docs)
+    return [doc for doc in related_docs if len(doc.page_content.strip()) > 50]
 
-def store(docs:list[Document]):
-    embeddings = HuggingFaceEmbeddings(model_name="/data/aifin/huggingface/GanymedeNil/text2vec-large-chinese",
+
+def store(docs: list[Document]):
+    docs = load_and_split(docs)
+    print("进入存储阶段")
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model_dict[EMBEDDING_MODEL],
                                        model_kwargs={'device': EMBEDDING_DEVICE})
-    tmp_vs_path = str(uuid.uuid4())
-    vector_store = temp_vector_store(tmp_vs_path, embeddings)
-    vector_store.chunk_size = CHUNK_SIZE
-    vector_store.chunk_conent = True
-    vector_store.score_threshold = VECTOR_SEARCH_SCORE_THRESHOLD
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    text_docs = text_splitter.split_documents(docs)
-    vector_store.add_documents(text_docs)
+    count = 0
+    obj = None
+    while True and count < 3:
+        try:
+            obj = Milvus.from_documents(
+                docs,
+                embeddings,
+                connection_args={"host": "8.217.52.63", "port": "19530"},
+                collection_name="aifin",
+            )
+            break
+        except Exception as e:
+            print(f"error,写入矢量库异常,{e}")
+            count += 1
+    if not obj:
+        raise Exception("写入矢量库异常")
+    print("写入矢量库over")
+
 
 #############################读取ES####################################
 from elasticsearch import Elasticsearch
@@ -80,5 +93,7 @@ def readFromES(code:str)->list[Document]:
                 print(f"length=0")
                 break
 
+
 if __name__ == '__main__':
-    readFromES('002594')
+    code = sys.argv[1]  # 股票代码
+    readFromES(code)
