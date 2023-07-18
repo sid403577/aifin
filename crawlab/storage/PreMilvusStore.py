99fde8fe9e829bbf7f22dea4f1b287e79348eb5a
sid403577 <sid403577@gmail.com>
2023-07-18 18:34:30 +0800

    矢量库

--- crawlab/storage/PreMilvusStore.py
+++ crawlab/storage/PreMilvusStore.py
@@ -12,7 +12,8 @@ embedding_model_dict = {
     "ernie-tiny": "nghuyong/ernie-3.0-nano-zh",
     "ernie-base": "nghuyong/ernie-3.0-base-zh",
     "text2vec-base": "shibing624/text2vec-base-chinese",
-    "text2vec": "/root/model/text2vec-large-chinese",
+    "text2vec": "/data/aifin/huggingface/GanymedeNil/text2vec-large-chinese",
+    #"text2vec": "/root/model/text2vec-large-chinese",
     "m3e-small": "moka-ai/m3e-small",
     "m3e-base": "moka-ai/m3e-base",
 }
