08d0e448b7852fd0afbe09a53c750cae8530ec25
sid403577 <sid403577@gmail.com>
2023-07-19 12:43:22 +0800

    fix

--- vectorstores/test.py
+++ vectorstores/test.py
@@ -1,14 +1,16 @@
 import csv
+import os
 
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from langchain.vectorstores import FAISS
 from vectorstores import MyFAISS, MyMilvus
+from langchain.embeddings import OpenAIEmbeddings
 
 codes = ['002594', '000063']
 column = 'text'
-query = '比亚迪'
+query = '迈瑞医疗'
 
 
 def load_file(file_path):
@@ -19,11 +21,15 @@ def load_file(file_path):
                 yield row['abstract']
 
 
+os.environ["OPENAI_API_KEY"] = "sk-kcfJcDXKztSEuMxaSqVjvuniMFIlz8HSr2xApuxivkNINiEc" #当前key为内测key，内测结束后会失效，在群里会针对性的发放新key
+os.environ["OPENAI_API_BASE"] = "https://key.langchain.com.cn/v1"
+os.environ["OPENAI_API_PREFIX"] = "https://key.langchain.com.cn"
+
 if __name__ == "__main__":
-    embeddings = HuggingFaceEmbeddings(model_name='GanymedeNil/text2vec-large-chinese',
+    embeddings = HuggingFaceEmbeddings(model_name='../huggingface/GanymedeNil/text2vec-large-chinese',
                                        model_kwargs={'device': 'cpu'})
     n = 1
-    max = 200
+    max = 2
     docs = []
     chunk_size = 1000
     for text in load_file("./test.csv"):
@@ -42,18 +48,28 @@ if __name__ == "__main__":
             m += 1
         docs += [Document(page_content=t) for t in texts]
 
-    vs = MyFAISS.from_documents(docs, embeddings)
-    print("myfaiss searching", len(docs))
-    ret_docs = vs.similarity_search_with_score(query, 10)
-    print("myfaiss searched", len(ret_docs))
-    for doc in ret_docs:
-        score = doc.metadata['score']
-        print(f"{score} {doc.page_content}")
+    # vs = MyFAISS.from_documents(docs, embeddings)
+    # print("myfaiss searching", len(docs))
+    # ret_docs = vs.similarity_search_with_score(query, 10)
+    # print("myfaiss searched", len(ret_docs))
+    # for doc in ret_docs:
+    #     score = doc.metadata['score']
+    #     print(f"{score} {doc.page_content}")
 
     vs = FAISS.from_documents(docs, embeddings)
     print("faiss searching", len(docs))
     ret_docs = vs.similarity_search_with_score(query, 10)
     print("faiss searched", len(ret_docs))
+    for s in ret_docs:
+        doc = s[0]
+        score = s[1]
+        print(f"{score} {doc.page_content}")
+
+    embeddings = OpenAIEmbeddings()
+    vs = FAISS.from_documents(docs, embeddings)
+    print("openai faiss searching", len(docs))
+    ret_docs = vs.similarity_search_with_score(query, 10)
+    print("openai faiss searched", len(ret_docs))
     for s in ret_docs:
         doc = s[0]
         score = s[1]
