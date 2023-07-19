d65b163fb9cf31be43f31be0a31fc832176afdc9
sid403577 <sid403577@gmail.com>
2023-07-19 12:44:52 +0800

    矢量库

--- api.py
+++ api.py
@@ -438,7 +438,7 @@ def docs2source(docs):
                                 "title": doc.metadata.get("title"),
                                 "url": doc.metadata.get("url"),
                                 "rootUrl": get_root_domain(doc.metadata.get("url")),
-                                "content": doc.page_content,
+                                "content": doc.metadata.get("abstract"),
                                 "date": doc.metadata.get("date"),
                                 "score": doc.metadata.get("score"),
                                 "source": doc.metadata.get("source"),
