ea0cdcbc40d51ca48b1cf45205489e5701c64854
sid403577 <sid403577@gmail.com>
2023-07-19 11:05:43 +0800

    fix

--- vectorstores/test.py
+++ vectorstores/test.py
@@ -3,12 +3,12 @@ import csv
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
-
+from langchain.vectorstores import FAISS
 from vectorstores import MyFAISS, MyMilvus
 
 codes = ['002594', '000063']
 column = 'text'
-query = '比亚迪怎么样'
+query = '比亚迪'
 
 
 def load_file(file_path):
@@ -20,10 +20,10 @@ def load_file(file_path):
 
 
 if __name__ == "__main__":
-    embeddings = HuggingFaceEmbeddings(model_name='../huggingface/GanymedeNil/text2vec-large-chinese',
+    embeddings = HuggingFaceEmbeddings(model_name='GanymedeNil/text2vec-large-chinese',
                                        model_kwargs={'device': 'cpu'})
     n = 1
-    max = 20
+    max = 200
     docs = []
     chunk_size = 1000
     for text in load_file("./test.csv"):
@@ -43,21 +43,30 @@ if __name__ == "__main__":
         docs += [Document(page_content=t) for t in texts]
 
     vs = MyFAISS.from_documents(docs, embeddings)
-    print("faiss searching", len(docs))
+    print("myfaiss searching", len(docs))
     ret_docs = vs.similarity_search_with_score(query, 10)
-    print("faiss searched", len(ret_docs))
+    print("myfaiss searched", len(ret_docs))
     for doc in ret_docs:
         score = doc.metadata['score']
         print(f"{score} {doc.page_content}")
 
-    vector_db = MyMilvus(
-        embeddings,
-        collection_name='cgf' + str(chunk_size),
-    )
-    vector_db.add_documents(docs)
-    print("milvus searching", len(docs))
-    ret_docs = vector_db.similarity_search_with_score(query, 10)
-    print("milvus searched", len(ret_docs))
-    for doc in ret_docs:
-        score = doc.metadata['score']
+    vs = FAISS.from_documents(docs, embeddings)
+    print("faiss searching", len(docs))
+    ret_docs = vs.similarity_search_with_score(query, 10)
+    print("faiss searched", len(ret_docs))
+    for s in ret_docs:
+        doc = s[0]
+        score = s[1]
         print(f"{score} {doc.page_content}")
+
+    # vector_db = MyMilvus(
+    #     embeddings,
+    #     collection_name='cgf' + str(chunk_size),
+    # )
+    # vector_db.add_documents(docs)
+    # print("milvus searching", len(docs))
+    # ret_docs = vector_db.similarity_search_with_score(query, 10)
+    # print("milvus searched", len(ret_docs))
+    # for doc in ret_docs:
+    #     score = doc.metadata['score']
+    #     print(f"{score} {doc.page_content}")
