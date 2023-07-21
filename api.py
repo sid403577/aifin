ac97e5ad079c98c49d912bdca0c4fff342e543b1
sid403577 <sid403577@gmail.com>
2023-07-21 18:08:50 +0800

    fix

--- api.py
+++ api.py
@@ -438,7 +438,7 @@ def docs2source(docs):
                                 "title": doc.metadata.get("title"),
                                 "url": doc.metadata.get("url"),
                                 "rootUrl": get_root_domain(doc.metadata.get("url")),
-                                "content": doc.metadata.get("abstract"),
+                                "content": doc.metadata["abstract"] if ('abstract' not in doc.metadata or not doc.metadata['abstract']) else doc.page_content,
                                 "date": doc.metadata.get("date"),
                                 "score": doc.metadata.get("score"),
                                 "source": doc.metadata.get("source"),
