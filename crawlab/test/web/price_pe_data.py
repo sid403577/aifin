d5e7461fdccd0d9244551cde7c881fb38829b6fb
sid403577 <sid403577@gmail.com>
2023-07-26 17:11:28 +0800

    crawlab

---
+++ crawlab/test/web/price_pe_data.py
@@ -0,0 +1,16 @@
+# 导入 efinance 库
+import efinance as ef
+
+
+def getPE_Price(code:str):
+    # 股票代码
+    # 获取股票的一些基本信息(返回 pandas.Series)
+    df= ef.stock.get_latest_quote(code)
+    print(df)
+    if not df.empty:
+        return df.get("最新价"), df.get("动态市盈率")
+
+if __name__ == '__main__':
+    result = getPE_Price("300750")
+    print(result[0])
+    print(result[1])
